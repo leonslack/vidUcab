5992243c3bc98352164dc1bc0e3bf01edad2758b
Christian Leon <christianleon@wattopoststudio.com>
2017-10-20 12:36:49 -0400

    wip

---
+++ src/main/java/com/ucab/vid/vidUcab/VidUcabApplication.java
@@ -0,0 +1,12 @@
+package com.ucab.vid.vidUcab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidUcabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidUcabApplication.class, args);
+	}
+}
