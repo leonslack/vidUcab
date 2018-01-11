48bcfe57812fe9175865e8eb7282a571899ca14b
Eduardo Lorenzo <ejlorenzo19@gmail.com>
2018-01-11 11:45:05 -0400

    war

--- src/main/java/com/ucab/restful/Application.java
+++ src/main/java/com/ucab/restful/Application.java
@@ -3,13 +3,20 @@ package com.ucab.restful;
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
 @EnableJSONDoc
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { 
+	    return application.sources(Application.class); 
+	} 
 }
